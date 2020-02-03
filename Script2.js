12208487d44e735578d6a7a7a6617d8d2a8f2b3f
Jagrati1998 <48794893+Jagrati1998@users.noreply.github.com>
2020-02-03 19:50:10 +0530

    Add files via upload

---
+++ Script2.js
@@ -0,0 +1,47 @@
+﻿// JavaScript source code
+// book class  represent a book 
+document.getElementById("#d").innerHTML = "hhh";
+
+class book {
+    constructor(title, auther, isbn) {
+        this.title = tittle;
+        this.auther = auther;
+        this.isbn = isbn;
+
+
+    }
+}
+//UI class handles storage
+class UI {
+    static displayBooks() {
+
+        const storedBooks = [
+            {
+                tittle: "one",
+                auther: "a",
+                isbn: "2342"
+
+            }
+        ];
+       
+        const books = storedBooks;
+        books.forEach((book) => UI.addBookToList(book));
+         
+        static function addBookToList(book) {
+            const List = document.querySelector("#body-list");
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${'#book.tittle'} </td>
+                            <td>${'#book.auther'} </td>
+                             <td>${'#book.isbn'} </td>`;
+
+                
+            List.appendChild(row);
+
+
+
+        }
+}
+}
+//store class: Display books
+//add a book
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
